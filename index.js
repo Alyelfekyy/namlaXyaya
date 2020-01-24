d1539960b1126177be0f282603381c6f1891e538
alynamlaa <alynamlaa@gmail.com>
2020-01-24 05:02:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 // get,post,put,delete
 const users = [
-    {  username: "OmarSherif",  password: "4"},
-    {  username: "Alimohamed",  password:"4"},
-    {  username: "yahiabadr",  password:"4"},
-    {  username: "3ebsooooo",  password:"4"}
+    {  username: "OmarSherif",  password: "4", grades:[]},
+    {  username: "Alimohamed",  password:"4", grades:[]},
+    {  username: "yahiabadr",  password:"4", grades:[]},
+    {  username: "3ebsooooo",  password:"4", grades:[]}
 ];
+// users[1].username="king";
+// users.push({username:"asadyalaa", password:"4", grades:[]});
+
+
 
 app.get('/', (request, response) => {
-    response.send(`<a href="/api/users">Namla</a>`);
+    response.send("Welcome");
 });
 
 app.get('/api/users', (request, response) => {
-   
+    
     response.send({data:users});
 });
 
@@ -29,5 +34,20 @@ app.get('/api/users/:username', (request, response) => {
     response.send({data:data || 'No student matches the requested id'});
 });
 
+app.post("/api/users",(request,response) =>{
+    
+    if(request.body.username===undefined)
+        response.send({error:"Missing username"});
+
+    if(request.body.password===undefined)
+        response.send({error:"Missing password"});
+     const username=request.body.username;
+     const password=request.body.password;
+    users.push({username:username, password:password,grades:[]});
+    response.send({msg:"User created",data:users});
+    
+
+     
+})
 const port = 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
